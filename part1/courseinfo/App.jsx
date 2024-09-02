b54b03571f590c848f056c2ce6e14747039b3519
Bianca Afonso <47660171+b7-afonso@users.noreply.github.com>
2024-09-02 14:52:33 +0200

    Update App.jsx

--- part1/courseinfo/App.jsx
+++ part1/courseinfo/App.jsx
@@ -48,11 +48,27 @@ const Total = () => {
 }
 
 const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
   return (
     <div>
-      <Header />
-      <Content />
-      <Total />
+    <Header course={course} />
+    <Content parts={parts} />
+    <Total parts={parts} />
     </div>
   )
 }
